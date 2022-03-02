c7a55dde287129d7f2d89ff4430fb7433f54b71a
GianRossini <gianrossini.developer@gmail.com>
2022-03-02 07:48:56 -0300

    creates account creation route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const express = require('express');
 
+const { v4: uuid } = require('uuid');
+
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body;
+  const id = uuid();
+
+  customers.push({
+    id,
+    name,
+    cpf,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
